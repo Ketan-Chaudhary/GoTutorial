250740570a013377a604ff1d77ea394ea8a58e25
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2025-04-27 10:49:54 +0530

    POST Request

---
+++ 21POSTRequest/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("POST Request")
+	PostRequestHandler()
+}
+
+func PostRequestHandler() {
+	const MyUrl = "http://localhost:8000/post"
+
+	// fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"Ketan":"Chaudhary",
+			"Learning":"GoLang"
+		}
+	`)
+
+	response, _ := http.Post(MyUrl, "application/json", requestBody)
+
+	var DataResponse strings.Builder
+	cnt, _ := io.ReadAll(response.Body)
+	byteData, _ := DataResponse.Write(cnt)
+	fmt.Println("Length of byteData", byteData)
+	fmt.Println("Data :", DataResponse.String())
+}
