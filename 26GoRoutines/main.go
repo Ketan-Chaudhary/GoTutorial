a8541b02347d2deda07f857d0714c23571a6ebb0
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2025-05-01 23:28:15 +0530

    Mutex

--- 26GoRoutines/main.go
+++ 26GoRoutines/main.go
@@ -10,6 +10,9 @@ import (
 // majorly we didn't create a variable
 // we should be creating a pointer to pass the reference ot the wait group
 var waitGroup sync.WaitGroup
+var mut sync.Mutex
+
+var signals = []string{"test"}
 
 func main() {
 	// go greeter("Hello")
@@ -28,7 +31,11 @@ func main() {
 
 	waitGroup.Wait() // always goes to the end of the main function or function being called
 	// make the main function wait unit all the goroutines finish their jobs
-
+	fmt.Println(signals)
+	// in this screnario this is working fine
+	// but there can be cases where multiple goroutines simulatenously try to write data into same memory block
+	// so for that situation we should use Mutex
+	// which lock the memory
 }
 
 // func greeter(s string) {
@@ -45,6 +52,10 @@ func getStatusCode(endpoint string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	mut.Lock()
+	signals = append(signals, endpoint)
+	mut.Unlock()
 
 	fmt.Printf("%d Status code for %s :\n", res.StatusCode, endpoint)
 
